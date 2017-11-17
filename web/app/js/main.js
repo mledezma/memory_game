989a4b6bce3b2ae63510f746c0d3b0bc7adc2aae
Marco Ledezma <marcoledezmacordero09@gmail.com>
2017-11-17 17:53:00 -0600

    Almost finished, still workin in the extra requirement, and in the reset of the index of the card that im currently clicking

--- web/app/js/main.js
+++ web/app/js/main.js
@@ -1,3 +1,5 @@
+//ToDo: Por cada carta que ya haya visto y no empareje perder 50 puntos
+//ToDo: Mejorar el volteo de la carta y la manera en que resetea el index de la carta en la que me encuentro
 var game = (function () {
   var frontCards = ['img/cards/1.jpg', 'img/cards/2.jpg', 'img/cards/3.jpg', 'img/cards/4.jpg', 'img/cards/5.jpg', 'img/cards/6.jpg', 'img/cards/7.jpg',  'img/cards/8.jpg', 'img/cards/9.jpg', 'img/cards/10.jpg'];  
   var currentCardId = 0;
@@ -6,23 +8,40 @@ var game = (function () {
   var previousCard;
   var currentScore = 0;
   var isMatching = false;
+  var cardsFlipped = 0;
+  var matchedCards = 0;
 
-  //Working
   function _deleteCard(card) {
-    console.log('Previous Card', previousCard);
-    console.log('Current Card', previousCard);
+    var cards = [previousCard, currentCard];
+    cards.forEach(function (card) {
+      card.removeEventListener('click', _displayCard);
+      card.classList.add('is-deleted');
+    });
+  }
+
+  function _displayWinMsg() {
+    if (matchedCards === frontCards.length/2) {
+      alert('You won');
+    }
+  }
+
+  function _updateScore() {
+    document.getElementById('currentScore').innerText = currentScore;
+    _displayWinMsg()
   }
 
   function _matchCard(cardId) {
     if (previousCardId === currentCardId) {
       isMatching = true;
       currentScore += 100;
+      matchedCards += 2;
+      _updateScore();
     } else {
       isMatching = false;      
       console.log('It didnt');
     }
   }
-  //ToDo: Por cada carta que ya haya visto y no empareje perder 50 puntos
+
   function _duplicateCards(front) {
     var cards = front.concat(front);
     return cards;
@@ -38,33 +57,46 @@ var game = (function () {
       currentCardId = 0;   
       card.classList.remove('is-horizontal-rotated');
       card.isClicked = false;
-    }, 3000);
+      cardsFlipped--;
+      setTimeout(function() {
+        card.querySelector('img').src = 'img/card-sample.jpg';
+      }, 500);
+    }, 2000);    
+  }
+
+  function _displayCard(card, index) {
+    if (cardsFlipped >= 2) {
+      return;
+    }
+    cardsFlipped++;      
+    if (!this.isClicked) {
+      var cardId = frontCards[this.index].replace('img/cards/', '').replace('.jpg', '');
+      var cardImg = this.querySelector('img');
+      previousCardId = currentCardId;
+      currentCardId = cardId;
+      previousCard = currentCard;
+      currentCard = this;
+      _matchCard(cardId);
+
+      if (isMatching) {
+        _deleteCard(this);
+        console.log('delete this cards');
+      }
+
+      cardImg.src = frontCards[this.index];
+      this.classList.add('is-horizontal-rotated');
+      _initialPosition(this);
+      this.isClicked = true;
+    } else {
+      console.log('Clikea otra carta');
+    }
   }
 
   function _flipCard(cards) {
     cards.forEach(function(card, index) {
       card.isClicked = false;
-      card.addEventListener('click', function () {
-        if(!this.isClicked) {
-          var cardId = frontCards[index].replace('img/cards/', '').replace('.jpg', '');
-          previousCardId = currentCardId;
-          currentCardId = cardId;
-          previousCard = currentCard;
-          currentCard = this;
-          _matchCard(cardId);
-          if(isMatching) {
-            _deleteCard(this);
-            console.log('delete this cards');
-          }
-          var cardImg = this.querySelector('img');
-          cardImg.src = frontCards[index];  
-          this.classList.add('is-horizontal-rotated');
-          _initialPosition(this);
-          this.isClicked = true;
-        } else {
-          console.log('Clikea otra carta');
-        }
-      });
+      card.index = index;
+      card.addEventListener('click', _displayCard);
     });
   }
 
@@ -78,7 +110,6 @@ var game = (function () {
   return {
     init: init
   }
-  //ToDo: Eliminar las cartas que emparejen.
 })();
 
 game.init();
