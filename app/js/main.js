0aa87e7a58ad36d911c8d731432c6a4769050e10
Marco Ledezma <marcoledezmacordero09@gmail.com>
2017-12-05 19:48:34 -0600

    Fixing issue with path of image

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,7 @@
 // ToDo: Que reste 50
 const game = (function () {
   let frontCards = [{src:'1.jpg', id:1},{src:'2.jpg', id:2},{src:'3.jpg', id:3},{src:'4.jpg', id:4},{src:'5.jpg', id:5},{src:'6.jpg', id:6},{src:'7.jpg', id:7},{src:'8.jpg', id:8},{src:'9.jpg', id:9},{src:'10.jpg', id:10}];  
-  let imagePath = '/../img/cards/';
+  let imagePath = 'img/cards/';
   let currentScore = 0;
   let isMatching = false;
   let matchedCards = 0;
