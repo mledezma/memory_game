a69508ef1e0e4840717d1c93316ec4c2a334b032
Marco Ledezma <marcoledezmacordero09@gmail.com>
2017-12-06 17:13:39 -0600

    .

--- app/js/main.js
+++ app/js/main.js
@@ -130,7 +130,7 @@ const game = (function () {
   function loadServiceWorker() {
     if ('serviceWorker' in navigator) {
       window.addEventListener('load', () => {
-        navigator.serviceWorker.register('../sw.js').then(registration => {
+        navigator.serviceWorker.register('sw.js').then(registration => {
           console.log('SW registered: ', registration);
         }).catch(registrationError => {
           console.log('SW registration failed: ', registrationError);
